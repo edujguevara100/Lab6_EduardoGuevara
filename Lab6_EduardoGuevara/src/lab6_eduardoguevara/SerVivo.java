0ede4e8d5548b4fb7d311205a7fda2f8ec9b1841
edujguevara100 <edujguevara100@gmail.com>
2017-11-24 14:46:19 -0600

    Ya se crean personas y se guardan en el archivo creado

---
+++ Lab6_EduardoGuevara/src/lab6_eduardoguevara/SerVivo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_eduardoguevara;
+
+/**
+ *
+ * @author edujg
+ */
+public class SerVivo {
+
+    private String raza;
+    private int ki;
+    private int maxage;
+    private String planeta;
+
+    public SerVivo() {
+    }
+
+    public SerVivo(String raza, int ki, int maxage, String planeta) {
+        this.raza = raza;
+        this.ki = ki;
+        this.maxage = maxage;
+        this.planeta = planeta;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getKi() {
+        return ki;
+    }
+
+    public void setKi(int ki) {
+        this.ki = ki;
+    }
+
+    public int getMaxage() {
+        return maxage;
+    }
+
+    public void setMaxage(int maxage) {
+        this.maxage = maxage;
+    }
+
+    public String getPlaneta() {
+        return planeta;
+    }
+
+    public void setPlaneta(String planeta) {
+        this.planeta = planeta;
+    }
+
+    @Override
+    public String toString() {
+        return "SerVivo{" + "raza=" + raza + ", ki=" + ki + ", maxage=" + maxage + ", planeta=" + planeta + '}';
+    }
+
+}
---
+++ Lab6_EduardoGuevara/src/lab6_eduardoguevara/Universo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_eduardoguevara;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author edujg
+ */
+public class Universo {
+
+    private String nombre;
+    private ArrayList<SerVivo> hab = new ArrayList();
+    private File archivo = null;
+
+    public Universo() {
+    }
+
+    public Universo(String nombre) {
+        this.nombre = nombre;
+        archivo = new File("./" + nombre + ".txt");
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre + ".txt";
+    }
+
+    public ArrayList<SerVivo> getHab() {
+        return hab;
+    }
+
+    public void setHab(ArrayList<SerVivo> hab) {
+        this.hab = hab;
+    }
+
+    public void setPersona(SerVivo a) {
+        hab.add(a);
+    }
+
+    @Override
+    public String toString() {
+        return "Universo{" + "nombre=" + nombre + ", hab=" + hab + '}';
+    }
+
+    public void guardar() throws IOException {
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            fw = new FileWriter(archivo, false);
+            bw = new BufferedWriter(fw);
+            for (SerVivo t : hab) {
+                bw.write(t.getRaza() + "|");
+                bw.write(t.getKi() + "|");
+                bw.write(t.getMaxage() + "|");
+                bw.write(t.getPlaneta() + "|");
+                bw.write("\n");
+            }
+            bw.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bw.close();
+        fw.close();
+    }
+
+    public void cargar() {
+        if (archivo.exists()) {
+            Scanner sc = null;
+            hab = new ArrayList();
+            try {
+                sc = new Scanner(archivo);
+                sc.useDelimiter("|");
+                while (sc.hasNext()) {
+                    hab.add(new SerVivo(sc.next(), sc.nextInt(), sc.nextInt(), sc.next()));
+                }
+
+            } catch (Exception e) {
+            }
+            sc.close();
+        }
+    }
+}
